35fc05032dcea39c4c6ef01f7ac201851649a307
SSemenkin <linkforptr@gmail.com>
2022-03-25 09:06:01 +0300

    4G-2G

--- MakeHO.py
+++ MakeHO.py
@@ -1,5 +1,6 @@
 import csv
 import re
+import itertools
 
 
 class MakeHO:
@@ -27,7 +28,6 @@ class MakeHO:
             for i in cells_to_ho:
                 for k in cell_from_google:
                     if str(k.CellId) in cells_to_ho[i]:
-                        print(i, k.CellId, k.Downlink, k.PSC)
                         file.writelines(f"RLUMC:CELL={i},ADD,UMFI={k.Downlink}-{k.PSC}-NODIV;\n")
                         file.writelines(f"RLNRI:CELL={i}, CELLR={k.CellId}, SINGLE;\n")
                 file.writelines(f"RLSUC:CELL={i},FDDMRR=1,QSC=7,QSCI=0,QSI=7,SPRIO=YES,FDDQMIN=6,FDDQOFF=0;\n"
@@ -65,14 +65,147 @@ class MakeHO:
                                     f"SIB11Ind=TRUE, SIB12Ind=FALSE, TpenaltyHcsReselect=D0, BlindHoFlag=FALSE,"
                                     f" NPrioFlag=FALSE, InterNCellQualReqFlag=FALSE, CLBFlag=FALSE;\n\n")
 
-    def make_3g_lte_ericsson(self):
-        pass
-
     def make_3g_lte_huawei(self):
-        pass
-
-    def make_2g_lte_ericsson(self):
         pass
 
     def make_2g_lte_huawei(self):
         pass
+
+    def make_4g_ericsson_2g(self, gsm_cells_info):
+        cells_to_ho = self.open_csv_file('Cell', ['Neighbour'])
+
+        def freq_neighbours(cells: dict, lte_cells: dict):
+            freq_group = """CREATE
+(
+ parent "ManagedElement=1,ENodeBFunction=1,GeraNetwork=1"
+ identity "GSM"
+ moType GeranFreqGroup
+ exception none
+ nrOfAttributes 1
+ frequencyGroupId Integer 1
+)"""
+            freqs_2g = ""
+            ext_2g_cells = ""
+            ext_geran_cell = ""
+            lte_reselect = ""
+            lte_hos = ""
+            for i in cells:
+                freqs_2g += f"""
+CREATE
+(
+ parent "ManagedElement=1,ENodeBFunction=1,GeraNetwork=1"
+ identity {cells[i]['BCCHNO']}
+ moType GeranFrequency
+ exception none
+ nrOfAttributes 3
+ arfcnValueGeranDl Integer {cells[i]['BCCHNO']}
+ bandIndicator Integer 0
+ geranFreqGroupRef Array Reference 1 "ManagedElement=1,ENodeBFunction=1,GeraNetwork=1,GeranFreqGroup=GSM"
+)\n"""
+                ext_2g_cells += f"""
+CREATE
+(
+ parent "ManagedElement=1,ENodeBFunction=1,GeraNetwork=1"
+ identity {cells[i]['CGI'].split('-')[-1]}
+ moType ExternalGeranCell
+ exception none
+ nrOfAttributes 6
+ plmnIdentity  Struct
+ 	nrOfElements 3
+ 		mcc Integer 255
+ 		mnc Integer 99
+ 		mncLength Integer 2
+ lac Integer {cells[i]['CGI'].split('-')[2]}
+ cellIdentity Integer {cells[i]['CGI'].split('-')[-1]}
+ bcc Integer {cells[i]['BSIC'][1:]}
+ ncc Integer {cells[i]['BSIC'][:1]}
+ geranFrequencyRef Reference "ManagedElement=1,ENodeBFunction=1,GeraNetwork=1,GeranFrequency={cells[i]['BCCHNO']}"
+)\n"""
+                ext_geran_cell += f"""
+SET
+(
+ mo "ManagedElement=1,ENodeBFunction=1,GeraNetwork=1,ExternalGeranCell={cells[i]['CGI'].split('-')[-1]}"
+ exception none
+ masterGeranCellId String "{cells[i]['CGI'].split('-')[-1]}"
+)
+
+SET
+(
+ mo "ManagedElement=1,ENodeBFunction=1,GeraNetwork=1,ExternalGeranCell={cells[i]['CGI'].split('-')[-1]}"
+ exception none
+ rimCapable Integer 2
+)
+"""
+            for i in lte_cells:
+                lte_reselect += f"""
+CREATE
+(
+ parent "ManagedElement=1,ENodeBFunction=1,EUtranCellFDD=1{i[-1:]}"
+ identity "GSM"
+ moType GeranFreqGroupRelation
+ exception none
+ nrOfAttributes 2
+ geranFreqGroupRef Reference "ManagedElement=1,ENodeBFunction=1,GeraNetwork=1,GeranFreqGroup=GSM"
+ cellReselectionPriority Integer 5
+)
+
+SET
+(
+ mo "ManagedElement=1,ENodeBFunction=1,EUtranCellFDD=1{i[-1:]},GeranFreqGroupRelation=GSM"
+ exception none
+ allowedPlmnList  Array Struct 1
+	nrOfElements 3
+		mcc Integer 255
+		mnc Integer 99
+		mncLength Integer 2
+)
+
+SET
+(
+ mo "ManagedElement=1,ENodeBFunction=1,EUtranCellFDD=1{i[-1:]},GeranFreqGroupRelation=GSM"
+ exception none
+ csFallbackPrio Integer 7
+)
+
+SET
+(
+ mo "ManagedElement=1,ENodeBFunction=1,EUtranCellFDD=1{i[-1:]},GeranFreqGroupRelation=GSM"
+ exception none
+ csFallbackPrioEC Integer 7
+)
+"""
+                for k in lte_cells[i]:
+                    lte_hos += f"""
+CREATE
+(
+ parent "ManagedElement=1,ENodeBFunction=1,EUtranCellFDD=1{i[-1:]},GeranFreqGroupRelation=GSM"
+ identity {re.findall('[0-9]+', k.replace('_', ''))[0] if 'LUG' in k else k.replace('D', '').replace('G', '')}
+ moType GeranCellRelation
+ exception none
+ nrOfAttributes 1
+ extGeranCellRef Reference "ManagedElement=1,ENodeBFunction=1,GeraNetwork=1,ExternalGeranCell={re.findall('[0-9]+', k.replace('_', ''))[0] if 'LUG' in k else k.replace('D', '').replace('G', '')}"
+)\n"""
+            return freq_group + f"\n{'/' * 75}\n" + freqs_2g + f"\n{'/' * 75}\n" + ext_2g_cells + f"\n{'/' * 75}\n" \
+                   + ext_geran_cell + f"\n{'/' * 75}\n" + lte_reselect + f"\n{'/' * 75}\n" + lte_hos
+
+        LTE_2g = {}
+        for i in cells_to_ho:
+            if i[:-2] in LTE_2g:
+                LTE_2g[i[:-2]].update({i: cells_to_ho[i]})
+            else:
+                LTE_2g[i[:-2]] = {i: cells_to_ho[i]}
+
+        for i in LTE_2g:
+            with open(f"{i}.txt", "w+") as file:
+                cells_2g = list(dict.fromkeys(list(itertools.chain(*LTE_2g[i].values()))))
+                cells_2g_info = {}
+                # Все соты 2г относящиеся к одной БС ЛТЕ
+                for k in cells_2g:
+                    k = k.replace('_1', 'A').replace('_2', 'B').replace('_3', 'C')
+                    cells_2g_info[k] = gsm_cells_info.get_cell_info(k)
+                file.writelines(freq_neighbours(cells_2g_info, LTE_2g[i]))
+
+    def make_3g_lte_ericsson(self, cell_from_google: dict):
+        cells_to_ho = self.open_csv_file('Cell', ['NEIGHBOUR.CELL_IDENTITY'])
+        for i in cells_to_ho:
+            print(i, cells_to_ho)
--- main.py
+++ main.py
@@ -8,17 +8,21 @@ excel = Table(client_account_file="grounded-pivot-261916-f78fb501c60b.json",
 # Получение значений с полей из 3G с заданными полями
 cells_3g = excel.get_cells_by_query('3G_NB', ['LAC', 'CellId', 'Uplink', 'Downlink', 'PSC'])
 
-# pool = ControllerPool()
-# pool.add_controller('BSC03', '172.25.157.99', 'Administrator', 'Administrator1@')
-# pool.add_controller('BSC04', '10.140.3.7', 'ts_user', 'apg43l2@')
-# pool.add_controller('BSC05', '10.140.27.68', 'ts_user', 'apg43l1@')
+pool = ControllerPool()
+pool.add_controller('BSC03', '172.25.157.99', 'Administrator', 'Administrator1@')
+pool.add_controller('BSC04', '10.140.3.7', 'ts_user', 'apg43l2@')
+pool.add_controller('BSC05', '10.140.27.68', 'ts_user', 'apg43l1@')
 
 # print("Я проверил таблицы 3g и 4g в Google Sheets перед тем, как делать хо.")
 # print("Создатели программы не несут ответственности за неправильно сделанные ХО. Все на свой страх и риск.")
 # type_of_HO = input('Type of HO?: \n2G-2G\t2G-3G\n3G-3G\t3G-2G\n')
 # file_path = input('Send file: ')
 # if type_of_HO == '3G-3G':
-file_path = 'C:\\Users\\admin\\PycharmProjects\\Handovers\\svr-3g-2g.csv'
+
+# file_path = 'C:\\Users\\admin\\PycharmProjects\\Handovers\\BSLUGLTE-3G.csv'
+file_path = 'C:\\Users\\admin\\PycharmProjects\\Handovers\\138-2g-4g.csv'
 
 ho = MakeHO(file_path)
-ho.make_3g_2g(cells_3g)
+
+ho.make_4g_ericsson_2g(pool)
+# ho.make_3g_lte_ericsson(cells_3g)
